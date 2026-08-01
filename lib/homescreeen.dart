25f1383ff3e2711101beb9a50497e1721e8f3b8a
khushbooedugaon-cell <khushboo.edugaon@gmail.com>
2026-08-01 14:35:46 +0530

    i added a home screen

---
+++ lib/homescreeen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Homescreeen extends StatelessWidget {
+  const Homescreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Home screen"),
+    );
+  }
+}
